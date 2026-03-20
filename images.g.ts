6c91306105032666b173279c79f968ca48c84f1f
willandelevenhopperbyers-afk <willandelevenhopperbyers@gmail.com>
2026-03-20 20:58:02 +1100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,51 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "HEART":return img`
+. . . . . . . . . . . . . . . . 
+. . 6 6 6 6 9 . . 9 6 6 6 9 . . 
+. 6 9 1 1 1 6 9 9 6 9 1 1 6 9 . 
+6 9 1 1 1 1 9 9 1 6 1 1 1 1 6 9 
+6 1 1 1 1 1 9 1 1 9 9 1 1 1 1 6 
+9 6 1 1 9 9 9 1 1 9 9 1 1 1 1 6 
+9 6 6 9 6 . . 9 9 . . 9 1 1 9 6 
+6 1 1 1 6 . . . . . . 6 9 9 6 9 
+6 1 1 1 9 6 . . . . 9 9 9 6 6 9 
+9 6 1 1 1 1 9 . . 6 1 1 1 9 6 9 
+. 9 9 9 1 1 1 9 6 6 1 1 1 1 9 9 
+. . 9 1 1 1 9 9 6 6 1 1 1 6 . . 
+. . . 6 1 6 9 1 1 9 9 6 6 . . . 
+. . . . 6 6 1 1 1 1 9 9 . . . . 
+. . . . . 9 6 1 1 6 9 . . . . . 
+. . . . . . 9 6 6 9 . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
